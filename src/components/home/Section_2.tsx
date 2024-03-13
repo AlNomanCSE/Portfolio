2c013e6b0790335d238b3f0d547d27f99276e279
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-03-13 19:16:32 +0600

    skills added to section2 and responsive

--- src/components/home/Section_2.tsx
+++ src/components/home/Section_2.tsx
@@ -7,24 +7,28 @@ const Section_2 = (props: Props) => {
   return (
     <section className={styles.section2}>
       <h1>My Top Skills</h1>
-      <div >
+      <div className={styles.images}>
         <Image
           src="https://upload.wikimedia.org/wikipedia/commons/3/38/HTML5_Badge.svg"
           alt="html"
           width={100}
           height={100}
+          className={styles.image}
         />
+
         <Image
           src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
           alt="html"
           width={100}
           height={100}
+          className={styles.image}
         />
         <Image
           src="https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg"
           alt="html"
           width={100}
           height={100}
+          className={styles.image}
         />
 
         <Image
@@ -32,18 +36,22 @@ const Section_2 = (props: Props) => {
           alt="html"
           width={100}
           height={100}
+          className={styles.image}
+        />
+
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Git-logo.svg"
+          alt="html"
+          width={100}
+          height={100}
+          className={styles.image}
         />
         <Image
           src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
           alt="html"
           width={100}
           height={100}
-        />
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Git-logo.svg"
-          alt="html"
-          width={100}
-          height={100}
+          className={styles.image}
         />
       </div>
     </section>
