49d38d92bc82e25ed5bad9317d90b00b29945e4f
AlNomanCSE <abdullahalnomancse@gmail.com>
2025-02-14 11:58:06 +0600

    Modified only section 2

--- src/components/home/Section_2.tsx
+++ src/components/home/Section_2.tsx
@@ -8,13 +8,8 @@ const Section_2 = (props: Props) => {
     <section className={styles.section2}>
       <h2>My Top Skills</h2>
       <div className={styles.images}>
-        {/* <Image
-          src="https://1000logos.net/wp-content/uploads/2020/09/Java-Emblem.jpg"
-          alt="html"
-          width={100}
-          height={100}
-          className={styles.image}
-        /> */}
+
+        <img src="https://logos-world.net/wp-content/uploads/2022/07/Java-Logo.png" alt="" height={500} width={500} className={styles.image}/>
         <Image
           src="https://upload.wikimedia.org/wikipedia/commons/4/44/Spring_Framework_Logo_2018.svg"
           alt="html"
@@ -22,23 +17,17 @@ const Section_2 = (props: Props) => {
           height={100}
           className={styles.image}
         />
-
+        <img src="https://www.devopsmadness.com/images/dockerventure_1/docker_logo.png" alt="" height={500} width={500} className={styles.image}/>
+       
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+          alt="html"
+          width={100}
+          height={100}
+          className={styles.image}
+        />
         <Image
           src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg"
-          alt="html"
-          width={100}
-          height={100}
-          className={styles.image}
-        />
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/3/38/HTML5_Badge.svg"
-          alt="html"
-          width={100}
-          height={100}
-          className={styles.image}
-        />
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
           alt="html"
           width={100}
           height={100}
@@ -52,21 +41,7 @@ const Section_2 = (props: Props) => {
           className={styles.image}
         />
         <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg"
-          alt="html"
-          width={100}
-          height={100}
-          className={styles.image}
-        />
-        <Image
           src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Git-logo.svg"
-          alt="html"
-          width={100}
-          height={100}
-          className={styles.image}
-        />
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
           alt="html"
           width={100}
           height={100}
