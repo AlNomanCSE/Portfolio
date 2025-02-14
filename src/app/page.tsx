f64dd398c7753b84e663189c85fb6c86c3079f98
AlNomanCSE <abdullahalnomancse@gmail.com>
2025-02-14 16:24:57 +0600

    Core Projects added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import styles from "./page.module.css";
 import Section_2 from "@/components/home/Section_2";
 import Section_3 from "@/components/home/Section_3";
 import Section_4 from "@/components/home/Section_4";
+import CoreProjects from "@/components/home/CoreProjects";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Section_1 />
       <Section_2 />
+      <CoreProjects />
       <Section_3 />
       <Section_4 />
     </main>
